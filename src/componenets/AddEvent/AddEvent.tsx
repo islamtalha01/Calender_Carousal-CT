aa1c7857f9e4dc03a7011f2cdfcd94e44a3614d5
talhacarbonteq01 <muhammad.talha@carbonteq.com>
2023-08-23 17:46:53 +0500

    Basic Dev of Add Event collapse
    Build basic panels in the collalpse of addevent

--- src/componenets/AddEvent/AddEvent.tsx
+++ src/componenets/AddEvent/AddEvent.tsx
@@ -1,32 +1,69 @@
-import React from 'react';
-import type { CollapseProps } from 'antd';
-import { Collapse } from 'antd';
 
+import type { CollapseProps } from 'antd';
+import { Collapse,Row,Button } from 'antd';
+// import { SettingOutlined } from '@ant-design/icons';
+import CurrentDay from '../CurrentDay/CurrentDay';
 
 const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
+  Hi
 `;
-
+const genExtra = () => (
+<CurrentDay/>
+);
+const Duration=()=>(
+  <>
+   <Button type="primary" shape="circle">
+  +
+</Button>
+<p>2:00</p>
+  <Button type="primary" shape="circle">
+  -
+</Button>
+  </>
+)
 const items: CollapseProps['items'] = [
   {
     key: '1',
-    label: 'This is panel header 1',
+    label: 'Date',
     children: <p>{text}</p>,
+    extra:genExtra()
+    
   },
   {
     key: '2',
-    label: 'This is panel header 2',
+    label: 'Time',
     children: <p>{text}</p>,
   },
   {
     key: '3',
-    label: 'This is panel header 3',
+    label: 'Duration',
     children: <p>{text}</p>,
+    extra:Duration(),
   },
 ];
 
-const App: React.FC = () => <Collapse accordion items={items} />;
+function AddEvent () {
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+return (
+  <Row style={{margin:'20px',justifyContent:'center'}}>
+       <Collapse accordion items={items} style={{width: "1000px"}} expandIconPosition='end' />
+  </Row>
+  
+)
+
+} 
+
+export default AddEvent;
