5c5238c55a9aaab5c432e944a8e8aa3791dc25b9
talhacarbonteq01 <muhammad.talha@carbonteq.com>
2023-08-24 16:56:51 +0500

    Added Carousal Fixed Box shadow

--- src/componenets/AddEvent/AddEvent.tsx
+++ src/componenets/AddEvent/AddEvent.tsx
@@ -1,69 +1,54 @@
-
-import type { CollapseProps } from 'antd';
-import { Collapse,Row,Button } from 'antd';
+import type { CollapseProps } from "antd";
+import { Collapse, Row, Button } from "antd";
 // import { SettingOutlined } from '@ant-design/icons';
-import CurrentDay from '../CurrentDay/CurrentDay';
-import DateCardList from '../DateCardList/DateCardList';
+import CurrentDay from "../CurrentDay/CurrentDay";
+import DateCardList from "../DateCardList/DateCardList";
 const text = `
   Hi
 `;
-const genExtra = () => (
-<CurrentDay/>
-);
-const Duration=()=>(
+const genExtra = () => <CurrentDay />;
+const Duration = () => (
   <>
-   <Button type="primary" shape="circle">
-  +
-</Button>
-<p>2:00</p>
-  <Button type="primary" shape="circle">
-  -
-</Button>
+    <Button type="primary" shape="circle">
+      +
+    </Button>
+    <p>2:00</p>
+    <Button type="primary" shape="circle">
+      -
+    </Button>
   </>
-)
-const items: CollapseProps['items'] = [
+);
+const items: CollapseProps["items"] = [
   {
-    key: '1',
-    label: 'Date',
+    key: "1",
+    label: "Date",
     children: <DateCardList/>,
-    extra:genExtra()
-    
+    extra: genExtra(),
   },
   {
-    key: '2',
-    label: 'Time',
+    key: "2",
+    label: "Time",
     children: <p>{text}</p>,
   },
   {
-    key: '3',
-    label: 'Duration',
+    key: "3",
+    label: "Duration",
     children: <p>{text}</p>,
-    extra:Duration(),
+    extra: Duration(),
   },
 ];
 
-function AddEvent () {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return (
-  <Row style={{margin:'20px',justifyContent:'center'}}>
-       <Collapse accordion items={items} style={{width:'800px'}} expandIconPosition='end' />
-  </Row>
-  
-)
-
-} 
+function AddEvent() {
+  return (
+    <Row style={{ margin: "20px", justifyContent: "center" }}>
+      <Collapse
+        accordion
+        items={items}
+        style={{ width:"600px" }}
+        expandIconPosition="end"
+      />
+    </Row>
+  );
+}
 
 export default AddEvent;
