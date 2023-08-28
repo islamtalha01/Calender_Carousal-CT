a5438df77e461c6796af33103fba28b685575313
talhacarbonteq01 <muhammad.talha@carbonteq.com>
2023-08-28 15:17:04 +0500

    Created Types and utilities Directories

---
+++ src/common/types/calendar.types.ts
@@ -0,0 +1,36 @@
+import { Dayjs } from "dayjs"
+
+export type Date = {
+    date: Dayjs
+  
+    /** @default false*/
+    closed: boolean
+  }
+
+/**@description Represents a closed date. It Can Either be (string) or a date (Dayjs) */
+export type ClosedDate = string | Dayjs
+
+
+export type selectedSlot = {
+  /** @default null */
+  date: Dayjs | null
+  /** @default null */
+  time: Dayjs | null
+//   /** @default [avg(minDuration, maxDuration).floor()] */
+  duration: number
+}
+export type Formats = {
+    /**
+     * @default "DD MMMM YYYY"
+     * @example "03 July 2023"
+     */
+    date: string
+    /**
+     * @default "hh:mm a"
+     * @example "12:00 pm"
+     */
+    time: string
+    /** @default "12h" */
+    clock: "12h" | "24h"
+  }
+  
